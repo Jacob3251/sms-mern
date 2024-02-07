f61909c793d4fb0f43b7a2bab4e71cda38457955
Md. Nayeem Hasan Adil <86750829+Jacob3251@users.noreply.github.com>
2024-02-07 18:09:01 +0600

    Basic CRUD Done

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -3,44 +3,62 @@
 
 const asyncHandler = require("express-async-handler");
 
+const Student = require("../model/studentModel");
+
 // @desc get students
 // @route GET /api/v1/student
 // @access Private
 const getStudent = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Get Students",
-  });
+  const students = await Student.find();
+  res.status(200).json(students);
 });
 
 // @desc set students
 // @route POST /api/v1/student/add
 // @access Private
 const setStudent = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  if (!req.body.name) {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({
-    message: "set Students",
+
+  const student = await Student.create({
+    name: req.body.name,
   });
+  res.status(200).json(student);
 });
 
 // @desc get students
 // @route PUT /api/v1/student/:id/update
 // @access Private
 const updateStudent = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "update Students",
-  });
+  const student = await Student.findById(req.params.id);
+  if (!student) {
+    res.status(400);
+    throw new Error("Student not found");
+  }
+
+  const updatedStudent = await Student.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.status(200).json(updatedStudent);
 });
 
 // @desc get students
 // @route DELETE /api/v1/student/:id/delete
 // @access Private
 const deleteStudent = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "delete Students",
-  });
+  const student = await Student.findById(req.params.id);
+  if (!student) {
+    res.status(400);
+    throw new Error("Student not found");
+  }
+  await student.deleteOne();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
